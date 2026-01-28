bbb5c7c240f67abadb6e405480bf0675858b7d2e
bosssuksit <bosssuksit03@gmail.com>
2026-01-28 12:30:23 +0700

    Commit 03 : ทำ Navigation Bar ที่ homeUI เสร็จแล้ว

--- lib/views/home_ui.dart
+++ lib/views/home_ui.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_body_health_calculator_app/views/about_ui.dart';
+import 'package:flutter_body_health_calculator_app/views/bmi_ui.dart';
+import 'package:flutter_body_health_calculator_app/views/bmr_ui.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeUi extends StatefulWidget {
   const HomeUi({super.key});
@@ -8,8 +12,60 @@ class HomeUi extends StatefulWidget {
 }
 
 class _HomeUiState extends State<HomeUi> {
+  int currentIndexStatus = 1;
+
+  //สร้างตัวแปรเก็บ widget
+  List<Widget> ShowBody = [
+    BmiUi(),
+    AboutUi(),
+    BmrUi(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFFDB3500),
+        title: Text(
+          'Body Health Calculator', 
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value){
+          setState(() {
+            currentIndexStatus = value;
+          });
+        },
+        currentIndex: currentIndexStatus,
+        selectedItemColor: Color(0xFFDB3500),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_2_outlined
+            ),
+            label: 'BMI',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.monitor_weight_outlined
+            ),
+            label: 'About',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              FontAwesomeIcons.personBooth
+            ),
+            label: 'BMR',
+          ),
+      
+        ],
+      ),
+      body: ShowBody[currentIndexStatus],
+      
+    );
   }
 }
